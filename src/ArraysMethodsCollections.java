163793abcee7294c3d4d3827974dec9ac2573612
Nereida Rondon <nereida.rondon5@gmail.com>
2022-11-13 21:05:58 -0500

    update

--- src/ArraysMethodsCollections.java
+++ src/ArraysMethodsCollections.java
@@ -49,16 +49,16 @@ public class ArraysMethodsCollections {
 		System.out.println("B. Concatenated names: " + concatNames);
 		
 		
-//3.	How do you access the last element of any array?
+//3.	How do you access the last element of any array?✔️
 		System.out.println("\n" + "#3:");
 		System.out.println("Last element of an array: " + ages[ages.length-1]);
 				
 		
-//4.	How do you access the first element of any array?
+//4.	How do you access the first element of any array?✔️
 		System.out.println("\n" + "#4:");		
 		System.out.println("First element of an array: " + ages[0]);
 				
-//5.	Create a new array of int called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
+//5.	Create a new array of int called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.✔️
 		System.out.println("\n" + "#5:");
 		
 		int[] nameLengths = new int[names.length];
@@ -71,27 +71,28 @@ public class ArraysMethodsCollections {
 				
 		
 		
-//6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+//6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.✔️
 		System.out.println("\n" + "#6:");
 		int sum=0;
 		for(int i=0; i< nameLengths.length; i++ ) {
 			sum += nameLengths[i];
 		}
 		System.out.println("Sum of elements: "+ sum );
-	
 		
+//7.		
 		System.out.println("\n" + "#7:");
 		myConcatMethod("Hello", 3);
 		
+//8.		
 		System.out.println("\n" + "#8:");
-		printFullName("Betty", "Boop");
+		printFullName("Betty", "Boop");	
 		
-		
+//9.		
 		System.out.println("\n" + "#9:");
 		int[] arr200 = {50, 60, 70, 20};
 		greaterThan100(arr200);
 		
-		
+//10.		
 		System.out.println("\n" + "#10:");
 		double [] array4 = {2.5, 7.5, 3.2, 1.8, 5.0};
 		System.out.println("Average of array1: " + averageOfArray(array4));
@@ -101,20 +102,22 @@ public class ArraysMethodsCollections {
 		double [] array313 = {6.45, 1.25, 4.15, 2.8, 1.0};
 		System.out.println("Average of array2: " + averageOfArray(array313));
 		
-		
+//11.		
 		System.out.println("\n" + "#11:");
 		greaterAverage(array4, array313);
 		System.out.println("Is array1 greater than array2? " + greaterAverage(array4, array313));
-	
+
+//12.
 		System.out.println("\n" + "#12:");
 		boolean isHotOutside=true;
 		double moneyInPocket= 11.00;
-		
+
 		System.out.println("isHotOutside = " + isHotOutside);
 		System.out.println("moneyInPocket = " + moneyInPocket);
 		
 		System.out.println("Both are true? " + willBuyDrink(isHotOutside, moneyInPocket));
 		
+//13.
 		System.out.println("\n" + "#13:");
 		double radius = 4.5;
 		System.out.println(findCircleArea(radius));
@@ -123,7 +126,7 @@ public class ArraysMethodsCollections {
 	
 //METHODS
 	
-//7.	Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in “Hello” and 3, I expect the method to return “HelloHelloHello”).
+//7.	Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in “Hello” and 3, I expect the method to return “HelloHelloHello”).✔️
 		
 	  static void myConcatMethod(String word, int n) {
 		      		
@@ -135,14 +138,14 @@ public class ArraysMethodsCollections {
 	  }
 		
 	  
-//8.	Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).
+//8.	Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).✔️
 	  
 	  static void printFullName(String firstName, String lastName){
 		 System.out.println("Full name: "+ firstName + " "+ lastName); 
 	  }
 	  
 	  
-//9.	Write a method that takes an array of int and returns true if the sum of all the ints in the array is greater than 100.
+//9.	Write a method that takes an array of int and returns true if the sum of all the ints in the array is greater than 100.✔️
 	  public static void greaterThan100(int[] array){
 		  int arraySum=0;
 		  for (int num : array) {
@@ -154,7 +157,7 @@ public class ArraysMethodsCollections {
 		  }
 	  };
 	  		
-//10.	Write a method that takes an array of double and returns the average of all the elements in the array.
+//10.	Write a method that takes an array of double and returns the average of all the elements in the array.✔️
 	  public static double averageOfArray(double[] arr){	
 
 		  double average = 0;
@@ -166,7 +169,7 @@ public class ArraysMethodsCollections {
 	  }
 	  
 	  
-//11.	Write a method that takes two arrays of double and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+//11.	Write a method that takes two arrays of double and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.✔️
 	  public static boolean greaterAverage(double[] arr1, double[] arr2){
 		  
 		  if(averageOfArray(arr1) > averageOfArray(arr2)) {
@@ -175,7 +178,7 @@ public class ArraysMethodsCollections {
 			  return false;
 	  }
 	  
-//12.	Write a method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+//12.	Write a method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.✔️
 	  public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
 		  if(isHotOutside && moneyInPocket > 10.5) {
 			  return true;
